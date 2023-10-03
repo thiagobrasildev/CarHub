d621b3b1b057e04a390ef7356f70226c1255d9f3
Thiago Brasil <thiagobrasildev@gmail.com>
2023-10-03 18:10:31 -0300

    created car component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
-import { CustomFilter, Hero, SearchBar } from "@/components";
+import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
+import { fetchCars } from "@/utils";
 
-export default function Home() {
+export default async function Home() {
+  const allCars = await fetchCars();
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
   return (
     <main className="overflow-hidden">
       <Hero />
@@ -15,10 +20,25 @@ export default function Home() {
           <SearchBar />
 
           <div className="home__filter-container">
-            <CustomFilter title="combustivel" />
-            <CustomFilter title="Ano" />
+            {/* <CustomFilter title="combustivel" />
+            <CustomFilter title="Ano" /> */}
           </div>
         </div>
+
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
+            </div>
+          </section>
+        ) : (
+          <div className="home__error-container">
+            <h2 className="text-black text-xl font-bold"> Ops, no results</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
       </div>
     </main>
   );
